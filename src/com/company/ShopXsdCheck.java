4687b4a8f62fabf1a73ac89625eb3ceb5cf45fe3
Viktoriia Krutovskaia <Viktoriia_Krutovskaia@epam.com>
2020-07-23 21:41:26 +0400

    [JavaIzhevskRD-xml] Add XSD validator class

---
+++ src/com/company/ShopXsdCheck.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.*;
+import java.io.*;
+
+
+
+public class ShopXsdCheck {
+
+    public static void main(String[] args) throws SAXException, IOException {
+        SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+
+        File schemaLocation = new File("src/com/company/shop.xsd");
+        Schema schema = factory.newSchema(schemaLocation);
+
+        Validator validator = schema.newValidator();
+
+        Source source = new StreamSource(args[0]);
+
+        try {
+            validator.validate(source);
+            System.out.println(args[0] + " is valid.");
+        } catch (SAXException ex) {
+            System.out.println(args[0] + "is not valid because ");
+            System.out.println(ex.getMessage());
+        }
+    }
+}
